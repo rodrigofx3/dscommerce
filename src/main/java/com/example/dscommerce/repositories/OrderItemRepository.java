2bcea70d3beaf71249e4c4e00e1e1fd98baae0d1
Rodrigo Faria Ramos <rodframos@outlook.com>
2024-12-17 16:32:00 -0300

    Update OrderItemRepository to use composite key for OrderItem entity

--- src/main/java/com/example/dscommerce/repositories/OrderItemRepository.java
+++ src/main/java/com/example/dscommerce/repositories/OrderItemRepository.java
@@ -1,7 +1,8 @@
 package com.example.dscommerce.repositories;
 
 import com.example.dscommerce.entities.OrderItem;
+import com.example.dscommerce.entities.OrderItemPK;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+public interface OrderItemRepository extends JpaRepository<OrderItem, OrderItemPK> {
 }
