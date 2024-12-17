e57238efa651d9ea94ced073e746951270c05bda
Rodrigo Faria Ramos <rodframos@outlook.com>
2024-12-17 10:35:54 -0300

    Implement UserDetails and GrantedAuthority in User and Role entities

--- src/main/java/com/example/dscommerce/entities/Role.java
+++ src/main/java/com/example/dscommerce/entities/Role.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
 
 @Entity
 @Table(name = "tb_role")
@@ -12,13 +13,16 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Data
 @EqualsAndHashCode(of = "id")
-public class Role {
+public class Role implements GrantedAuthority {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String authority;
 
-
+    @Override
+    public String getAuthority() {
+        return authority;
+    }
 
 }
